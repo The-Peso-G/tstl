34f3487ea600ac9b7dd88da8b43f0a05eb45c07d
Samchon <sam@myrealplan.co.kr>
2019-10-25 10:17:22 +0900

    Fixing #45

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,7 @@
 //================================================================ 
 /** @module std */
 //================================================================
-/**
- * Basic, internal features.
- */
-export import base = require("./base");
+export * from "./module";
 
-/**
- * Experimental Features.
- */
-export import experimental = require("./experimental");
-
-export * from "./container";
-export * from "./iterator";
-export * from "./algorithm";
-
-export * from "./exception";
-export * from "./functional";
-export * from "./numeric";
-export * from "./thread";
-export * from "./utility";
+import m = require("./module");
+export default m;
---
+++ src/test/test_default.ts
@@ -0,0 +1,6 @@
+import std from "..";
+
+export function test_default(): void 
+{
+    new std.Vector();
+}
